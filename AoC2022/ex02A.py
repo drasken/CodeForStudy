31047bbbc43aa41620aa87c6950438920158fe7a
drasken <turchi.r@outlook.com>
2023-08-08 23:55:33 +0200

    Ex 02 first half done

--- AoC2022/ex02A.py
+++ AoC2022/ex02A.py
@@ -7,7 +7,7 @@
 #(0 if you lost, 3 if the round was a draw, and 6 if you won).
 
 with open(file="input02A.txt", mode="r") as file:
-    stringaS = file.readlines()
+    stringaS = file.readlines() #the string read from the txt file
     responses = []
     res = 0
     for i in stringaS:
@@ -20,9 +20,29 @@ with open(file="input02A.txt", mode="r") as file:
             res += 2
         elif k == 'Z':
             res += 3
-#still miss points draws and wins
 
-   
+    points_game = []
+    for j in stringaS:
+        points_game.append(j.rstrip("\n"))
+
+    for n in points_game:#this cycle count the points for each game
+        if n == "A X" or n == "B Y" or n =="C Z":
+            res += 3
+        elif n == "A Y" or n == "B Z" or n == "C X":
+            res += 6
+        else:
+            continue            
+
+    print(res)
 
     #res1 = 41566, wrong too high
     #res2 = 20938, wrong too high
+    #res3 = 12535 OK!!
+    
+    #SECOND PART
+    """
+    The Elf finishes helping with the tent and sneaks back over to you. 
+    "Anyway, the second column says how the round needs to end: 
+    X means you need to lose, Y means you need to end the round in a 
+    draw, and Z means you need to win. Good luck!"
+    """
