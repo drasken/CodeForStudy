43c51adff25a994cdbf132ca045512de648ab1d1
drasken <75556556+drasken@users.noreply.github.com>
2023-10-10 23:12:11 +0200

    2nd milestone done

--- Exercises/gameOfLife.py
+++ Exercises/gameOfLife.py
@@ -12,33 +12,75 @@ The cell then updates its own liveness according to 4 rules:
 
 import random
 
-#firts milestone
+#Firts milestone
 
 def deadState (width, height):
-    line = [0 for i in range(width)]
-    output = [line for j in range(height)]
+    output = [[0 for i in range(width)] for j in range(height)]
 
     return output
 
-#IN PROGRESS
-
+#Done
 def randomState (board, probability = 0.5):
 
-    for li in board:
-        for j in range(len(li)):
+    width = len(board)
+    height = len(board[0])
+
+    for i in range(width):
+        for j in range(height):
 
             rand = random.random()
             if rand >= probability:
-                li[j] = 1
+                board[i][j] = 1
             else:
-                li[j] = 0    
+                board[i][j] = 0    
 
+    
 
+#Second Milestone
+
+def printLine(lisNum):
+    line = ''
+    for i in lisNum:
+        if i == 1:
+            line += '#'
+        elif i == 0:
+            line += ' '
+        else:
+            continue     
+    return line       
+
+#DONE
+
+def render(state):
+    width = len(state[0])
+    upBottomLines = '-' * width
+    newState = ''
+    for i in state:
+        newPrintedLine = '|' + printLine(i) + '|\n'
+        newState += newPrintedLine
+    print(' ' + upBottomLines + ' ')
+    print(newState)
+    print(' ' + upBottomLines + ' ')
+
+#DONE
+
+#Third Milestone
+
+def next_board_state(state):
+    pass
 
 
 board = deadState(10,10)
+board[2][4] = 6
+board[2][6] = 6
+board[2][8] = 6
 print(board)
+
+bo = board [0] is board [2]
+print(bo)
 randomState(board)
 test = [x for x in range (10)]
 
 print (board)
+
+render(board)
