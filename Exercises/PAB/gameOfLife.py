ee96975605e853ac76f4f7f94bf0f45cbc2943eb
drasken <contumax90@yahoo.it>
2023-10-17 16:01:14 +0200

    Worked on milestone 3

--- Exercises/PAB/gameOfLife.py
+++ Exercises/PAB/gameOfLife.py
@@ -67,24 +67,52 @@ def render(state):
 #Third Milestone
 
 #probably need to implement a checkIfAlive function
-def next_board_state(state): #use next function for this 
-    newState = deadState(len(state[0]), len(state))
+def next_board_state(state): #use next function for 
+    width = len(state)
+    height = len(state[0])
+    newState = deadState(width, height )
+    for i in range(width):
+        for j in range(height):
+            #here the idea is tu return a 1 or 0 depending of the state
+            # with the function checkCellState
+            indexCell = [i, j]
+            newState[i][j] = checkCellState(state,state[i][j])
     return newState                     
 
-def checkCellState(cell, nearCellsCount): #return the int nu tu put in the tab
+def checkCellState(board, cell): #return the int nu tu put in the tab
     """
+    Check for dead or alive neighbors.
+    Cell is a List of index for the position of the Cell in the Grid.
     Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
     Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
     Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
     Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
 
     """
-    if cell == 0:
+    aliveCells = 0
+    #Pseudocodice per me, da cancellare
+    # ad ogni check se tovo un 1 aggiungi += 1 a aliveCells
+    #if cell[0] == 0: #first row
+    #  if cell[1] == 0 or == len(stat[0]):
+    #    chech for angle
+    #elif cell[1] == 0 or come sopra:
+    #  sono in un borso laterale
+    #elif: #come per la prima file ma invertita
+    #  check solo per 5 posizioni
+    #elif:
+    # check per le posizioni normali, 8 vicini
+    #here start counting
+
+    #if DA IMPLEMENTARE 
+    
+
+    
+    if aliveCell == 0:
         if nearCellsCount == 3:
             return 1
         else:
             return 0
-    elif cell == 1:
+    elif aliveCell == 1:
         if nearCellsCount == 0 or nearCellsCount == 1:
             return 0
         if nearCellsCount == 2 or nearCellsCount == 3:
