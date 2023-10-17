9aac2fe07495ee0a57abbf981adf51edf6fbfd19
drasken <contumax90@yahoo.it>
2023-10-17 19:51:12 +0200

    started simple alarm project

---
+++ Exercises/simpleAlarm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 19:11:16 2023
+
+@author: drasken
+"""
+
+# Space to define functions
+
+
+# Space for the if main thing
+
+if __name__ == '__main__':
+    pass
+    
