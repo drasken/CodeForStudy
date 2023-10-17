b1c5e39e6b5260e65c8571d541294b53a64d2064
drasken <contumax90@yahoo.it>
2023-10-17 19:52:05 +0200

    using this file as reference for simple CLI programs

---
+++ Exercises/simpleCLIbyRP.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 19:49:42 2023
+
+@author: drasken
+"""
+
+# ls_argv.py
+
+import sys
+from pathlib import Path
+
+if (args_count := len(sys.argv)) > 2:
+    print(f"One argument expected, got {args_count - 1}")
+    raise SystemExit(2)
+elif args_count < 2:
+    print("You must specify the target directory")
+    raise SystemExit(2)
+
+target_dir = Path(sys.argv[1])
+
+if not target_dir.is_dir():
+    print("The target directory doesn't exist")
+    raise SystemExit(1)
+
+for entry in target_dir.iterdir():
+    print(entry.name)
