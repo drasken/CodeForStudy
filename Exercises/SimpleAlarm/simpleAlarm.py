5bf8fc45fb23e3a36e824bc6ff136516da846c4f
drasken <contumax90@yahoo.it>
2023-10-30 20:34:15 +0100

    Organized in the same directory

---
+++ Exercises/SimpleAlarm/simpleAlarm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 19:11:16 2023
+
+@author: drasken
+"""
+
+import argparse
+import time
+
+
+# Space to define functions
+
+
+# Space for the if main thing
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='simpleAlarm', 
+                                     description='A simple CLi tool for starting an alarm using a terminal program after a given amount of minutes')
+    #here the rest of the code
+    
+
+
+
+if __name__ == '__main__':
+    pass
+    
