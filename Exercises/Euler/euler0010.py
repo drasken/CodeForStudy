e96f5094a2a6b9a32494179332a61091d20cc47f
drasken <contumax90@yahoo.it>
2023-11-10 00:06:49 +0100

    Euler 0010 doing.

--- Exercises/Euler/euler0010.py
+++ Exercises/Euler/euler0010.py
@@ -1,3 +1,9 @@
+"""The sum of the primes below 10  is 2+3+5+7 = 17.
+
+Find the sum of all the primes below two million.
+
+"""
+
 def checkPrimeBrute(num : int) -> bool:
     """
     reutilized from ex 0007
@@ -16,14 +22,28 @@ listSolutions : list = [1]
 #         listSolutions.append(indexCheck)
 #     indexCheck += 1
 
-numbersRange : list = [x for x in range(2000000)]
+#changed approach, using sieve of Eratostene
 
-def trimListPrimes(numbersRange):
-    listPrimesDiscovered : list = [1]
-    for i in numbersRange:
-        pass
+#adding only odd numbers
+numbersRange : list = [x for x in range(2,2000000) if x % 2 != 0 or x == 2]
+
+#print(numbersRange)
+#remove numbers if multiple of another
+#it works until 10000
+for i in numbersRange:
+    for j in numbersRange[1:]:
+        if j != i and j % i == 0:
+            numbersRange.remove(j)
+
+print (len(numbersRange))
+print(numbersRange[-1])
+
+# def trimListPrimes(numbersRange):
+#     listPrimesDiscovered : list = [1]
+#     for i in numbersRange:
+#         pass
         
         
 
-print(sum(listSolutions[1:-1]))
+#print(sum(listSolutions[1:-1]))
         
