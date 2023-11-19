06d93ce47528d77d42ee64301b2926edfaefeff0
drasken <contumax90@yahoo.it>
2023-11-19 18:34:01 +0100

    Upgraded

--- Exercises/Euler/euler0010.py
+++ Exercises/Euler/euler0010.py
@@ -4,46 +4,66 @@ Find the sum of all the primes below two million.
 
 """
 
-def checkPrimeBrute(num : int) -> bool:
-    """
-    reutilized from ex 0007
-    """
-    for i in range(2,num):
-        if num % i == 0:
-            return False
-    return True
 
-indexCheck : int = 2
-listSolutions : list = [1]
-#too much time to calculate with brute force
+listPrimes = [x for x in range(2,100000) if x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0 and x % 11 != 0 and x % 13 != 0 and x % 17 !=0 and x % 19 != 0 and x % 23 != 0 and x % 29 != 0]
+listPrimes = [2,3,5,7,11,13,17,19,23,29] + listPrimes
+index = 0
+for x in listPrimes[index:]:
+    for i in listPrimes[index+1:]:
+        if i % x == 0:
+            listPrimes.remove(i)#print(x)
+    index += 1
 
-# while listSolutions[-1] < 10:
-#     if checkPrimeBrute(indexCheck) == True:
-#         listSolutions.append(indexCheck)
-#     indexCheck += 1
+print(listPrimes)
+print(sum(listPrimes))
+print(len(listPrimes))
 
-#changed approach, using sieve of Eratostene
 
-#adding only odd numbers
-numbersRange : list = [x for x in range(2,2000000) if x % 2 != 0 or x == 2]
+## INSERT HERE FUNCTION USE THIS LOGIC
+# CHECK IN THE LIST
+# for each number try from the last, if
+#               from the start if divido ultimo per primo e dà resto remove() dalla lista
 
-#print(numbersRange)
-#remove numbers if multiple of another
-#it works until 10000
-for i in numbersRange:
-    for j in numbersRange[1:]:
-        if j != i and j % i == 0:
-            numbersRange.remove(j)
+# def checkPrimeBrute(num : int) -> bool:
+#     """
+#     reutilized from ex 0007
+#     """
+#     for i in range(2,num):
+#         if num % i == 0:
+#             return False
+#     return True
 
-print (len(numbersRange))
-print(numbersRange[-1])
+# indexCheck : int = 2
+# listSolutions : list = [1]
+# #too much time to calculate with brute force
 
-# def trimListPrimes(numbersRange):
-#     listPrimesDiscovered : list = [1]
-#     for i in numbersRange:
-#         pass
+# # while listSolutions[-1] < 10:
+# #     if checkPrimeBrute(indexCheck) == True:
+# #         listSolutions.append(indexCheck)
+# #     indexCheck += 1
+
+# #changed approach, using sieve of Eratostene
+
+# #adding only odd numbers
+# numbersRange : list = [x for x in range(2,2000000) if x % 2 != 0 or x == 2]
+
+# #print(numbersRange)
+# #remove numbers if multiple of another
+# #it works until 10000
+# for i in numbersRange:
+#     for j in numbersRange[1:]:
+#         if j != i and j % i == 0:
+#             numbersRange.remove(j)
+
+# print (len(numbersRange))
+# print(numbersRange[-1])
+
+# # def trimListPrimes(numbersRange):
+# #     listPrimesDiscovered : list = [1]
+# #     for i in numbersRange:
+# #         pass
         
         
 
-#print(sum(listSolutions[1:-1]))
+# #print(sum(listSolutions[1:-1]))
         
