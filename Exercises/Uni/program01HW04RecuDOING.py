f30ee05e06d3b4d61076eb5718e1a5767aca95c3
drasken <contumax90@yahoo.it>
2023-12-02 18:27:42 +0100

    Created docstring for remaining function

--- Exercises/Uni/program01HW04RecuDOING.py
+++ Exercises/Uni/program01HW04RecuDOING.py
@@ -211,12 +211,72 @@ def studenti_brillanti(dbsize): #works, just timeout error, need refactoring
     pass
 
 def stampa_verbale(exam_code, dbsize, fileout):
+    """
+    stampa_verbale(exam_code, dbsize, fileout), che riceve un identificatore
+      di esame e salva nel fileout le informazioni relative
+      all'esame indicato, usando la seguente formula
+        "Lo studente <stud_surname> <stud_name>, matricola <stud_code>, ha sostenuto in data <date> l'esame di <course_name> con il docente <teach_surname> <teach_name> con votazione <grade>."
+      La funzione ritorna il voto dell'esame associato
+      all'identificatore ricevuto in input.
+
+    Args:
+        exam_code (TYPE): DESCRIPTION.
+        dbsize (TYPE): DESCRIPTION.
+        fileout (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
     pass
 
 def stampa_esami_sostenuti(stud_code, dbsize, fileout):
+    """
+    stampa_esami_sostenuti(stud_code, dbsize, fileout), che riceve un
+      numero di stud_code e salva nel file fileout la lista degli esami
+      sostenuti dallo studente identificato dal valore stud_code.
+      Le righe nel file devono essere ordinate in modo crescente
+      per data di esame sostenuto e, in caso di stessa data, in ordine
+      alfabetico. Il file ha una riga iniziale con il testo
+       "Esami sostenuti dallo studente  <stud_surname> <stud_name>, matricola <stud_code>",
+      mentre le righe seguenti hanno la seguente struttura
+        "<course_name>\t<date>\t<grade>", in cui i campi sono allineati
+      rispetto al nome del corso più lungo (ovvero tutte le date e
+      i voti sono allineati verticalmente). La funzione ritorna
+      il numero di esami sostenuti dallo studente.
+
+    Args:
+        stud_code (TYPE): DESCRIPTION.
+        dbsize (TYPE): DESCRIPTION.
+        fileout (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
     pass 
 
 def stampa_studenti_brillanti(dbsize, fileout):
+    """
+    stampa_studenti_brillanti(dbsize, fileout), che salva nel file
+      fileout una riga per ogni studente con una media di esami
+      sostenuti superiore o uguale a 28. Le righe nel file
+      devono essere ordinate in modo decrescente per media e,
+      in caso di parità, in ordine lessicografico per il
+      cognome e il nome dello studente.
+      Le righe nel file hanno la seguente struttura:
+          "<stud_surname> <stud_name>\t<media>", in cui il valore media
+      è allineato verticalmente per tutte le righe. La funzione
+      ritorna il numero di righe salvate nel file.
+
+    Args:
+        dbsize (TYPE): DESCRIPTION.
+        fileout (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
     pass
 
 
