d2e3d8f62dcf59783bdef924f96bd10925964c8a
Rudy <rudy@PF1ZYDMJ.homenet.telecomitalia.it>
2024-02-21 00:06:47 +0100

    Started working on puzzle of day 02

---
+++ AoC2023/day002.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 00:05:29 2024
+
+@author: drasken
+"""
+"""
+Determine which games would have been possible if the bag had been loaded with 
+only 12 red cubes, 13 green cubes, and 14 blue cubes.
+What is the sum of the IDs of those games?
+"""
+
