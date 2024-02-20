63e167a37cfda96311b0a4b871ed7b3206c16897
Rudy <rudy@PF1ZYDMJ.homenet.telecomitalia.it>
2024-02-21 00:53:24 +0100

    Written possible algorithms

--- AoC2023/day002.py
+++ AoC2023/day002.py
@@ -11,3 +11,16 @@ only 12 red cubes, 13 green cubes, and 14 blue cubes.
 What is the sum of the IDs of those games?
 """
 
+"""
+Possibile idee per calcolare analizzare le stringhe:
+    prendi la stringa sa sopo i 2 punti
+    prendi ogni set usando cone separatore il punto e virgola
+    prendi come separatore le virgole
+    dovrebbe essermi rimaste tutte substring del tipo --23 Colore--
+    cerco di estrarre il numero dalla stringa(modulo da importare??)
+    check con le condizione dell'indovinello
+    se ok aggiungi 1 al counter
+    ritorna counter
+    
+
+"""
