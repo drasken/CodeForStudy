281dc48e7cf85e635a8760580820b9fab989a6d3
drasken <contumax90@yahoo.it>
2024-02-25 23:35:43 +0100

    Solved part 1 of the puzzle
    * AoC2023/day002.py:

--- AoC2023/day002.py
+++ AoC2023/day002.py
@@ -36,19 +36,117 @@ def readInput(inputFile):
         context = f.readlines() #read file each str is a game
         for line in context:
             yield line #yield the str corrisponding on the line
-           
+ 
+
+def analyzeGames(fileName :str, colors: dict) -> int:
+    """ Analyze a game by each subset and if all true return the ID int
+    
+    """
+    
+    resIds = []
+    
+    fileReader = readInput(fileName)
+    for linea in fileReader:
+        processedLine = divideGameBySets(linea)
+        processedLine = divideSetByCubes(processedLine)
+        isPlayable = checkIfPlayableGame(processedLine, colors)
+        if isPlayable:
+            gameId = linea[5: linea.index(':')]
+            resIds.append(int(gameId))
+        
+    # isPlayable = False
+    
+    # gameId = 0
+    
+    # newList = divideGameBySets(lineOfFile)
+    # newList = divideSetByCubes(newList)
+    
+    # isPlayable = checkIfPlayable(newList)
+    
+    return sum(resIds)
+    pass
+
+def divideGameBySets(gameString:str) -> list:
+    
+    gameString = gameString[gameString.index(':') + 2:]
+    gameString = gameString.split(';')
+    return gameString
+    pass
+
+def divideSetByCubes(setOfCubes: list[str]) -> list[list]:
+    
+    resList = [stringa.split(',') for stringa in setOfCubes]
+    
+    return resList
+    pass
             
+def checkIfPlayableSet(gamesAnalyzed: list, colors: dict) -> bool:
+    
+    setBoolRes = set()                
+                    
+    for stringa in gamesAnalyzed:
+        for colore in colors:
+            if colore in stringa:
+                score = int(stringFunction.getNumberFromString(stringa))
+                if score <= colors[colore]:
+                    setBoolRes.add(True)
+                else:
+                    setBoolRes.add(False)
+    
+    return all(setBoolRes)
+        
+    pass
+
+def checkIfPlayableGame (gameList: list, colors: dict) -> list[bool]:
+    
+    resSetBool = set()
+    
+    for miniList in gameList:
+        getBool = checkIfPlayableSet(miniList, colors)
+        resSetBool.add(getBool)
+        
+    return all(resSetBool)
 
 
 if __name__ == '__main__':
     #need to process this names
-    cubesOfGmes = '12 red cubes, 13 green cubes, and 14 blue cubes'
+    cubesOfGmes = {'red': 12, 'green': 13, 'blue': 14}
 
     fileInput = 'input002.txt'
     provaReadFile = readInput('input002.txt')
     provaGen = next(provaReadFile)
     provaGen2 = next(provaReadFile)
-    provaSplit = provaGen2.split(';')
+    # provaSplit = provaGen2.split(';')
+    # provaSplit[0] = provaSplit[0][provaSplit[0].index(':') +2 : ]
+    # provaSplitSub = provaSplit[0]
+    # vediamo = stringFunction.getNumberFromString(provaSplitSub)
+    testFunction = 'asldk 8988 asdòlasòlkasd  90 asdklk 00'
+    testFunctionProva = stringFunction.getNumberFromString(testFunction)
+    
+    provaFuncDivide = divideGameBySets(provaGen)
+    provaFuncDivide2 = divideSetByCubes(provaFuncDivide)
+    provaFunPlayabe = checkIfPlayableGame(provaFuncDivide2, cubesOfGmes)
+    
+    inputAoC1 = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
+    inputAoC1 = divideGameBySets(inputAoC1)
+    inputAoC1 = divideSetByCubes(inputAoC1)
+    testAoC = checkIfPlayableGame(inputAoC1, cubesOfGmes)
+    
+    inputAoC2 = 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'
+    testAoC2 = checkIfPlayableGame(inputAoC2, cubesOfGmes)
+    
+    inputAoC3 = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+    inputAoC3 = divideGameBySets(inputAoC3)
+    inputAoC3 = divideSetByCubes(inputAoC3)
+    testAoC3 = checkIfPlayableGame(inputAoC3, cubesOfGmes)
+    
+    inputAoC4 = 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red'
+    testAoC4 = checkIfPlayableGame(inputAoC4, cubesOfGmes)
+    
+    inputAoC5 = 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+    testAoC5 = checkIfPlayableGame(inputAoC5, cubesOfGmes)
+    
+    testSerious = analyzeGames(fileInput, cubesOfGmes) #2162 DONE, CORRECT!!
     
 
     
--- AoC2023/stringFunction.py
+++ AoC2023/stringFunction.py
@@ -6,9 +6,24 @@ Created on Sun Feb 25 17:04:52 2024
 @author: drasken
 """
 
-def extractNumber(stringa):
-    """
+#FUNCRION NOT WORING SOMETIMES INFINITE LOOP
+def getNumberFromString(stringa:str) -> str:
+    """ Given a string in wich there is a nuber return the first number found"""
     
+    for index, char in enumerate(stringa):
+        if char.isdigit():
+            stringa[index:]
+            step = 1
+            while stringa[index: index + step].isdigit():
+                step +=1
+            return stringa[index: index + step]
+    
+    return '' #if no number is found
+
+
+def extractNumberForColor(stringa):
+    """
+    check if substring of desired colors appear and than find the number preceding it 
 
     Parameters
     ----------
@@ -17,7 +32,14 @@ def extractNumber(stringa):
 
     Returns
     -------
-    None.
+    dictColors: a dict of boolean, if all true the game is ok
 
     """
-    return 'prova ' + stringa
+    
+    listColors = ['red', 'green', 'blue']
+    
+    for color in listColors:
+        stringa.find(color)
+    
+    dictColors = {}
+    return dictColors
