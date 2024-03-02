9596b7b584c18d749fe4796ffa2f27aa62fe288f
drasken <contumax90@yahoo.it>
2024-03-02 18:45:03 +0100

    Project Euler problem 0009 solved

--- Exercises/Euler/euler0009.py
+++ Exercises/Euler/euler0009.py
@@ -5,31 +5,34 @@ Ex. 3^2 + 4^2 = 5^2
 There exists exactly one Pythagorean triplet for which a+b+c = 1000
 Find the product a*b*c
 """
+#Problem SOLVED!
 
+#functin to check if triplet is pythagorean
 def checkPythagorean(numA, numB, numC):
-    sommaSquare = numA ** numA + numB ** numB
-    if sommaSquare == numC ** numC:
-        return numA,numB,numC
+    if (numA**2) + (numB**2) == (numC ** 2):
+        return True
     else:
-        return 0
+        return False
+   
+
+#function to find triplets for specific sum
+
+def findPytTriplets (limitSum, listRes):
+    for numA in range(1, limitSum //2 +1): #enough to check to half + 1
+        for numB in range(numA+1, limitSum//2 + 1):
+            for numC in range(numB+1, limitSum//2 + 1):
+                if numA + numB + numC == limitSum:
+                    if checkPythagorean(numA, numB, numC):
+                        tripletTuple = (numA, numB, numC)
+                        listRes.append(tripletTuple)
+
+
+
+
+if __name__ == '__main__':
+    listTri = [] #all triplets
+    findPytTriplets(1000, listTri)
+    resProduct = lambda x: x[0] * x[1] * x[2]
+    result = resProduct(listTri[0]) #It worked!! 
     
-triSum = 1000
-
-indexA,indexB,indexC = 0,0,0
-
-def findTriplets(limitSum, num1, num2, num3):
-    checkFunc = checkPythagorean(num1, num2, num3)
-    if checkFunc == limitSum:
-        return num1* num2 * num3
-    else:
-        return -1
-
-res = 0    
-
-for i in range(501):
-    resTemp =  findTriplets(triSum,indexA,indexB,indexC)
-    if resTemp != -1:
-        res = resTemp
-    indexA, indexB, indexC += 1
-
-print(res)
+    
