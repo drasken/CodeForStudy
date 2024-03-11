22606c826ec4e0f0c1b3f4c9b13122b42a299607
drasken <contumax90@yahoo.it>
2024-03-11 16:38:41 +0100

    added stub function to Timer class

--- SimplePomodoroTimer/timer.py
+++ SimplePomodoroTimer/timer.py
@@ -11,13 +11,18 @@ Created on Mon Mar 11 16:13:28 2024
 import time
 
 class Timer:
-    
+    """
+    The class used to represent object Timer in the main function for the pomodoro script.
+    """
 
     def __init__(self):
         self.start = time.time(), time.ctime()
     
     def __str__(self):
         return f'The timer started at {self.start[1]}'
+    
+    def ringBellOnEnding(self): #to implement, ring the bell when the countdown ends
+        pass
         
 if __name__ == '__main__':
     testTimer = Timer()
