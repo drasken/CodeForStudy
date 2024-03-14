41e4691b84d3a5185e1f0bd91065740062830478
drasken <contumax90@yahoo.it>
2024-03-14 13:25:48 +0100

    Added pause() function in pomodoro file

--- SimplePomodoroTimerFunc/pomodoro.py
+++ SimplePomodoroTimerFunc/pomodoro.py
@@ -7,9 +7,15 @@ Created on Mon Mar 11 20:58:37 2024
 """
 
 import time
+from playsound import playsound
+
+#TODO:
+#After pause function maybe refactor a generic timer function?
+#Than a startRoutine function to start the study session
 
 def pomodoro(minutes: int = 30)->str:
     """
+    This function run a single pomodoro session
 
     Parameters
     ----------
@@ -18,11 +24,41 @@ def pomodoro(minutes: int = 30)->str:
 
     Returns
     -------
-    str
+    str: The message to tell the Pomodoro is ended
 
     """
     
-    countDown = minutes
+    countDown:int = minutes
+    convertionToSeconds:int = minutes * 60
+    start:float = time.time()
+    prevEnding:float = start + convertionToSeconds
+    
+    while True:
+        try:
+            current_time:float = time.time() #
+            current_time_h:str = time.ctime()
+            print(f"Now is: {current_time_h}. Still {countDown} minutes remaining")
+            countDown -= 1
+            if current_time >= prevEnding:
+                print('Pomodoro ended')
+                #testing playing sound
+                playsound('mixkit-digital-clock-digital-alarm-buzzer-992.wav')
+                break
+        except KeyboardInterrupt:
+            print("countDown interrupted!")
+            break
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+            break
+        time.sleep(60)
+    
+    return "Pomodoro Ended"
+
+
+def pause(minutes: int = 5)->str:
+    pass
+    
+    #this repeat pomodoro function, refactor(?)
     convertionToSeconds: int = minutes * 60
     start: float = time.time()
     prevEnding: float = start + convertionToSeconds
@@ -30,19 +66,20 @@ def pomodoro(minutes: int = 30)->str:
     while True:
         try:
             current_time: float = time.time()
-            current_time_h = time.ctime()
-            print(f"Now is: {current_time_h}. Still {countDown} minutes remaining")
-            countDown -= 1
+            current_time_h:str = time.ctime()
+            print(f'Now is {current_time_h}')
             if current_time >= prevEnding:
-                print('Pomodoro ended')
+                print('Break ended.')
+                playsound('mixkit-digital-clock-digital-alarm-buzzer-992.wav')
                 break
         except KeyboardInterrupt:
-            print("countDown interrupted!")
+            print("Break interrupted")
+            break
+        except Exception as e:
+            print(f"Exception occurred: {e}")
             break
         time.sleep(60)
-    
-    
-    return "Pomodoro Ended"
+    return "Break Ended"
     
 if __name__ == '__main__':
     provaPomodoro = pomodoro(1)
