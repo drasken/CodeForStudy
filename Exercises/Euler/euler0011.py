9a32ce8fb1146cc2debd33ecdcb285cc2b7e2fe0
drasken <contumax90@yahoo.it>
2024-04-13 19:18:50 +0200

    Solver PE problem 0011

--- Exercises/Euler/euler0011.py
+++ Exercises/Euler/euler0011.py
@@ -29,9 +29,9 @@ def check_horizontally(matrix:list[list], step:int) -> int:
             if (index + step) > len(row):
                 break
             else:
-                temp_sum = sum(row[index: index + step])
-                if temp_sum > max_found:
-                    max_found = temp_sum
+                temp_sum = util.prod_mine(row[index: index + step])
+                max_found = max(max_found, temp_sum)
+
                     
     return max_found
 
@@ -43,65 +43,70 @@ def check_vertically(matrix:list[list], step: int) -> int:
         if (row_index + step) > len(matrix):
             break
         for index, num in enumerate(row):
-           temp_sum = []
-           for el in matrix[row_index: row_index + step][index]:
-               temp_sum.append(el)
-           temp_sum = sum(temp_sum)
-           if temp_sum > max_found:
-               max_found = temp_sum
+           temp_sum = [matrix[row_index][index]]
+           count = 1
+           while count < step:
+               temp_sum.append(matrix[row_index + count][index])
+               count += 1
+           temp_sum = util.prod_mine(temp_sum)
+           max_found = max(max_found, temp_sum)
+
            
     return max_found
 
-def chack_diagonally(matrix:list[list], step:int) -> int:
+def check_diagonally(matrix:list[list], step:int) -> int:
+    
+    max_found = 0
+    
+    for row_index, row in enumerate(matrix):
+        if (row_index + step) > len(matrix):
+            break
+        for index, num in enumerate(row):
+            if (index + step) > len(row):
+                break
+            count = 1
+            temp_sum = [matrix[row_index][index]]
+            while count < step:
+                temp_sum.append(matrix[row_index + count][index + count])
+                count += 1
+            temp_sum = util.prod_mine(temp_sum)
+            max_found = max(max_found, temp_sum)
+    
+    return max_found
+    pass
+
+def check_diagonally_inverse(matrix: list[list], step:int) -> int:
+    
+    mirror_matrix = [row[::-1] for row in matrix]
+    
+    res = check_diagonally(mirror_matrix, step)
+    
+    return res
+            
+
+def find_max (matrix:list[list], step:int) -> int:
     
     pass
-            
-        
+
+    
+    hor_value, vert_value, diag_value, diag_mirror = check_horizontally(matrix, step), check_vertically(matrix, step), check_diagonally(matrix, step), check_diagonally_inverse(matrix, step)
+    
+    max_value = max(hor_value, vert_value, diag_value, diag_mirror)
+    
+    return max_value
 
 
 if __name__ == '__main__':
     testFile = transform_data(util.read_from_file('euler0011_input.txt'))
-    #testFile2 = transform_data(testFile)
-    # testMul = util.check_horizontally(testFile2, 5, 5, 4)
     
-    # max_temp = 0
-    
-    # sol = list()
-    
-    # hor_limit = (len(testFile2) - 4)
-    # for row_index, row in enumerate(testFile2):
-    #     for col_index, el in enumerate(row[:hor_limit]): 
-    #         if el == 0:
-    #             continue
-    #         else:
-    #             hor = util.check_horizontally(testFile2, row_index, col_index, 4)
-    #             if hor[0] > max_temp:
-    #                 max_temp = hor[0]
-    #                 sol = hor[1]
-    
-    # #NOT WORKING!!!
-    # ver_limit = (len(testFile2) - 4)
-    # for row_index, row in enumerate(testFile2[:ver_limit]):
-    #     for col_index, el in enumerate(row): 
-    #         if el == 0:
-    #             continue
-    #         else:
-    #             ver = util.check_vertically(testFile2, row_index, col_index, 4)
-    #             if ver[0] > max_temp:
-    #                 max_temp = ver[0]
-    #                 sol = ver[1]
-                    
-            
-    # diag_limit = (len(testFile2) - 4)
-    # for row_index, row in enumerate(testFile2[:diag_limit]):
-    #     for col_index, el in enumerate(row[:diag_limit]): 
-    #         if el == 0:
-    #             continue
-    #         else:
-    #             ver = util.check_vertically(testFile2, row_index, col_index, 4)
-    #             if ver[0] > max_temp:
-    #                 max_temp = ver[0]
-    #                 sol = ver[1]
-                    
-    
+    test_hor = check_horizontally(testFile, 4)
 
+    test_vert = check_vertically(testFile, 4)
+
+    test_diag = check_diagonally(testFile, 4)
+
+    
+    test_diag_mirr = check_diagonally_inverse(testFile, 4)
+
+    res = find_max(testFile, 4)
+    
