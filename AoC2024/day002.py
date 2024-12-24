c4ebbdccee8a23fae1a1919ccb10944824394111
drasken <contumax90@yahoo.it>
2024-12-24 14:46:15 +0100

    Started work on day 02

---
+++ AoC2024/day002.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 14:43:11 2024
+
+@author: drasken
+"""
+
+# Idea: check input for valid lines and write off the unvalid ones
+# than check for input_filtered length and we have the solution
