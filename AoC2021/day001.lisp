f8f360f86b4531d93160d414b93325fedd3156a7
drasken <contumax90@yahoo.it>
2025-01-15 19:28:01 +0100

    Completed day 2 both parts and got 2 stars

--- AoC2021/day001.lisp
+++ AoC2021/day001.lisp
@@ -16,4 +16,15 @@
 			    (setf acc (1+ acc))))
     acc))
 
-; First Part Solution: 1581 --> OK!!
+;; First Part Solution: 1581 --> OK!!
+
+(defun sol-part-2 (lis)
+  (sol-part-1
+    (loop for x from 1 to (- (length lis) 2)
+	  collect (reduce #'+ (list (nth (1- x) lis)
+				(nth x lis)
+				(nth (1+ x) lis))))))
+
+;; Second Part Solution: 1618 -> OK!!
+    
+
