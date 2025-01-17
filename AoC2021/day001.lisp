163c3e79f3b94f09049b85773d234a1b52e9ca89
drasken <contumax90@yahoo.it>
2025-01-17 23:24:43 +0100

    Added refactoring on AoC2021 day001

--- AoC2021/day001.lisp
+++ AoC2021/day001.lisp
@@ -16,6 +16,11 @@
 			    (setf acc (1+ acc))))
     acc))
 
+;;found online
+
+(defun increasing-pairs (nums)
+  (loop for (a b) on nums while b count (< a b))) ;;OK
+
 ;; First Part Solution: 1581 --> OK!!
 
 (defun sol-part-2 (lis)
@@ -24,6 +29,11 @@
 	  collect (reduce #'+ (list (nth (1- x) lis)
 				(nth x lis)
 				(nth (1+ x) lis))))))
+
+;;found online same as for part 1, same way using loop
+
+(defun increasing-tri (nums)
+  (loop for (a b c d) on nums while d count (< (+ a b c) (+ b c d))))
 
 ;; Second Part Solution: 1618 -> OK!!
     
