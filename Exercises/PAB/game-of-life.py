84e32c302f7071e6f7235a8c560110553be87fda
drasken <contumax90@yahoo.it>
2025-05-06 17:01:13 +0200

    Progressing in the game of life python version third milestone

--- Exercises/PAB/game-of-life.py
+++ Exercises/PAB/game-of-life.py
@@ -67,12 +67,50 @@ Algo: make a new board, check old board neighbors
 """
 
 
-def count_neighbors():
-    pass
+def count_neighbors(board: list[list], y_pos: int, x_pos: int) -> int:
+    """
+    Use this function to calculate next state of a cell in a board
+    Return 1 if alive else 0 if dead 
+    """
 
-def next_board_state(board):
-    pass
+    cell_state: int = board[y_pos][x_pos]  # must be 0 or 1
+    assert cell_state >= 0 and cell_state <= 1
     
+    new_state: int = 0
+
+    count_neigh : int = 0
+
+    for row in board[y_pos - 1: y_pos + 2]:
+        for el in row[x_pos - 1: x_pos + 2]:
+            if el != 2:
+                count_neigh += el
+    count_neigh -= cell_state
+
+    assert count_neigh >= 0 and count_neigh <= 8
+
+    if cell_state == 0 and count_neigh == 3:
+        new_state = 1
+        return new_state
+
+    elif cell_state == 1:
+        if count_neigh == 2 or count_neigh == 3:
+            new_state = 1
+            return new_state
+    else:
+        new_state = 0
+        return new_state
+
+# Tests passed
+
+def next_board_state(board: list[list]) -> list[list]:
+    """
+    Given in input a board return in output a new board state
+    First create a new board
+    Calculate next state for each cell in the old board
+    update the new board with new cell state
+    return the new board
+    """
+    pass
     
     
 
@@ -87,4 +125,13 @@ def main(h: int, w: int):
 if __name__ == '__main__':
 
     test = main(15, 30)
-    
+
+    # # test count neigh
+    # test_matrix = [[2,2,2,2], [2,0,1,0], [2,1,1,0],[2,0,1,0],[2,0,1,0]]
+    # test_count1 = count_neighbors(test_matrix, 1, 1)  # expected 1
+    # print(test_count1)
+    # test_count2 = count_neighbors(test_matrix, 2, 2)  # expected 1
+    # print(test_count2)
+    # test_count3 = count_neighbors(test_matrix, 3, 1)  # expected 0
+    # print(test_count3)
+
