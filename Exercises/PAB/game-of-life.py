9048502cbc0efbddddd1914695aee6a1a133ef3e
drasken <contumax90@yahoo.it>
2025-05-06 23:04:53 +0200

    Almost finished 4th milestone

--- Exercises/PAB/game-of-life.py
+++ Exercises/PAB/game-of-life.py
@@ -3,6 +3,8 @@ Trying again do it using Python
 """
 
 import random
+import time
+import copy
 
 # First Milestone
 
@@ -11,6 +13,10 @@ def dead_state(height: int, width: int) -> list[list[int]]:
     output = [[0 for i in range(width + 2)] for j in range(height + 2)]
 
     return output
+
+# test function
+# test_b = dead_state(5, 5)
+# print(test_b)
 
 
 def random_state(board: list, probability: float = 0.5) -> None:
@@ -33,6 +39,11 @@ def random_state(board: list, probability: float = 0.5) -> None:
                 if rand >= probability:
                     board[index_row][index_el] = 1  # else is already 0
 
+
+# test func
+# random_state(test_b)
+# print("testc")
+# print(test_b)
 
 # Done
 
@@ -88,19 +99,23 @@ def count_neighbors(board: list[list], y_pos: int, x_pos: int) -> int:
 
     assert count_neigh >= 0 and count_neigh <= 8
 
-    if cell_state == 0 and count_neigh == 3:
-        new_state = 1
-        return new_state
+    if cell_state == 0:
+        if count_neigh == 3:
+            return 1
+        else:
+            return 0
 
-    elif cell_state == 1:
+    if cell_state == 1:
         if count_neigh == 2 or count_neigh == 3:
-            new_state = 1
-            return new_state
-    else:
-        new_state = 0
-        return new_state
+            return 1
+        else:
+            return 0
+
+    if cell_state != 0 and cell_state != 1:
+        return -1
 
 # Tests passed
+
 
 def next_board_state(board: list[list]) -> list[list]:
     """
@@ -110,9 +125,25 @@ def next_board_state(board: list[list]) -> list[list]:
     update the new board with new cell state
     return the new board
     """
-    pass
-    
-    
+
+    h, w = len(board), len(board[0])  # TODO: type annotations??
+
+    out_board: list[list] = dead_state(h, w)
+
+    random_state(out_board)
+
+    for i_row, row in enumerate(board):
+        for i_el, el in enumerate(row):
+            if el != 2:
+                new_state = count_neighbors(board, i_row, i_el)
+                out_board[i_row][i_el] = new_state
+
+
+    return out_board
+
+# DONE
+# Fourth Milestone
+# Modify the main() function to loop infinitely
 
 def main(h: int, w: int):
 
@@ -121,10 +152,45 @@ def main(h: int, w: int):
 
     render(my_board)
 
+    # print("test!!!!")
+
+    # board2 = next_board_state(my_board)
+
+    # render(board2)
+    while True:
+        try:
+            board2 = next_board_state(my_board)
+
+            render(board2)
+
+            my_board = copy.deepcopy(board2)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("Goodbye!")
+            break
+
+    #     try:
+    #         my_board = dead_state(h, w)
+    #         random_state(my_board)
+
+    #         render(my_board)
+
+    #         # print("test!!!!")
+
+    #         board2 = next_board_state(my_board)
+
+    #         render(board2)
+
+    #         time.sleep(0.5)
+
+    #     except KeyboardInterrupt:
+    #         print("You stopped the program. Goodbye!")
+    #         break
+
 
 if __name__ == '__main__':
 
-    test = main(15, 30)
+    test = main(10, 20)
 
     # # test count neigh
     # test_matrix = [[2,2,2,2], [2,0,1,0], [2,1,1,0],[2,0,1,0],[2,0,1,0]]
@@ -134,4 +200,5 @@ if __name__ == '__main__':
     # print(test_count2)
     # test_count3 = count_neighbors(test_matrix, 3, 1)  # expected 0
     # print(test_count3)
+    # PASS
 
