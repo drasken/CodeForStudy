54b935d6a7ff613f282b8147c1e0c2c3b14b7cbe
drasken <contumax90@yahoo.it>
2025-07-11 00:32:38 +0200

    Implemented a simple plotting for the compound returns

--- Languages/Racket/compound-interest.rkt
+++ Languages/Racket/compound-interest.rkt
@@ -1,8 +1,57 @@
 #lang racket
 
+(require plot) ;; Used to plot returns
+(plot-new-window? #t)
 
-(define (compound-interest init-sum years interest)
-  (/ (round (* 100(* init-sum (expt (+ 1 interest) years)))) 100 ))
+;; How to procede:
+;; - Take a number of N time periods T-n
+;; - Create a list of length N + 1
+;; - The first element at time T-0 is the starting capital
+;; - The function should compute the resulting value at each time T-x
+;; - Let's use recursion and memoization to compare performance
 
 
-(define (compound-rec init-sum years interest deposit)) ;;TODO
+;;; Global Variables
+;; the time period of interest
+(define periods (range 30))
+(define my-rate 0.07)
+(define my-sum 10000)
+
+
+;; Just to test
+(define (simple-interest val rate)
+  (/ (round (* (+ val (* val rate)) 100)) 100))
+
+
+;; Funtion to calculate compound interest
+(define (calc-int val rate per)
+  (map (lambda (num)
+	 (/ (round (* (* (expt (add1 rate) num) val) 100)) 100))
+       per))
+
+
+(define my-inv (calc-int my-sum  my-rate periods)) ; Simple implementation, it works
+
+
+;; Create a list of points for plotting with lines function
+(define points (map (lambda (x y) (list x y)) periods my-inv))
+
+
+;; test plot library
+;; (plot (function sqr -2 2) #:title "Test") ;;this works
+
+;; plot with my-inv var
+(plot (lines points  #:color "red")
+      #:title "Test")
+
+
+
+
+
+;; (plot (list
+;;        (lines my-inv
+;; 	      #:color "yellow")
+;;        (points my-inv
+;; 	       #:fill-color "red")
+;;       #:x-min 0 #:x-max 20 #:title "Expected return"))
+
