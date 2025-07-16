173c3fef2f1e8cdb2afa06130346be055ffc6909
drasken <contumax90@yahoo.it>
2025-07-17 01:44:16 +0200

    Started L-99, 99 problems in Lisp (Racket)

---
+++ Languages/Racket/L99Problems/prob01.rkt
@@ -0,0 +1,39 @@
+;;;; Trying to do L-99: 99 problems in Lisp
+
+;; Problem 001
+(define (my-last lst)
+  (last lst))
+
+;; test
+(my-last '(a b c d))  ; OK
+
+
+;; Problem 002
+(define (my-but-last lst)
+  (cond [(< (length lst) 2) "error"]
+	[(= (length lst) 2) lst]
+	[else (my-but-last (cdr lst))]))
+
+(my-but-last '(1 2 3 4))  ; OK
+
+
+;; Problem 003
+; trying not to use a bult-in function
+(define (element-at lst k)
+  (cond [(empty? lst) "Errore"]
+	[(zero? k) (car lst)]
+	[else (element-at (cdr lst) (- k 1))]))
+
+;tests --> OK
+(element-at '(1 4 6 8 9) 3)
+(element-at '(1 4 6 8 9) 13)
+(element-at '(1 4 6 8 9) 0)
+
+
+;; Problem 004
+;; Find the number of elements in a list
+;; NO built-in function
+(define (my-len lst)
+  ("TODO"))
+
+   
