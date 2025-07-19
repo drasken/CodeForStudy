4e28daa46ab6f672c3d4019aea1f32a4ead67f82
drasken <contumax90@yahoo.it>
2025-07-20 00:20:42 +0200

    Doing them in CL

--- Languages/CommonLisp/L99Problems/prob001.lisp
+++ Languages/CommonLisp/L99Problems/prob001.lisp
@@ -1,1 +1,26 @@
-;;; Test
+;;;; Problem 001
+
+(defun my-last (lst)
+  "Find last element in a list"
+  (cond ((null lst) nil)
+	((null (cdr lst)) lst)
+	(t (my-last (cdr lst)))))
+
+;; test
+(my-last '(3 5 6 7))
+(my-last '())
+(my-last '(37))
+
+
+(defun my-last (lst)
+  "Find last two element in a list"
+  (cond ((null lst) nil)
+	((null (cdr lst)) nil)
+	((null (cddr lst)) lst)
+	(t (my-last (cdr lst)))))
+
+
+;;test
+(my-last '(3 5 6 7))
+(my-last '())
+(my-last '(37))
