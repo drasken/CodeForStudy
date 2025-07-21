1d5b98540d3ceb6a1a44f54b0e5a3d27a2074449
drasken <contumax90@yahoo.it>
2025-07-21 18:01:14 +0200

    Starting this toy project Double Files Finder

---
+++ Languages/CommonLisp/ScriptDoubleFiles/double-files-finder.lisp
@@ -0,0 +1,15 @@
+;; This file is the main file to make a script that searcha a directory tree and:
+;; - From a starting directory check all files
+;; - Get a digest for each files and stores it (still undecided how)
+;; - Does it recursively for each sub directory
+;; - When it's done, write on a log each path for files that produced same digest
+;; Optional features:
+;; - Choose a level of depth for the recursive search
+;; - Let the user stop the search while doing it from keyboard
+;; - Maybe let the user choose the starting directory not just the CWD
+;; - Maybe let the user choos the name and location for log file (check location)
+
+
+;; Here main function
+(defun main ()
+  )
