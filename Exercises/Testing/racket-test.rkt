37d8b4fac850d6b940a951b2091a89eb2e629a1a
drasken <contumax90@yahoo.it>
2025-07-27 10:48:21 +0200

    Reorganizing files and deleting unused old directory

--- Exercises/Testing/racket-test.rkt
+++
@@ -1,6 +0,0 @@
-;;;; Testing closures for functions
-
-(define my-var 666)
-(displayln my-var)
-
-
---
+++ Languages/Racket/Script/find-file.rkt
@@ -0,0 +1,5 @@
+;;;; First attempt
+
+;; Input a name (maybe later regex) anf fin all occurrences that match
+
+;; TODO
