6ad9f8b54d663b596ba018fa2dd0987df87d9185
drasken <contumax90@yahoo.it>
2025-08-03 11:16:47 +0200

    Done problem 018 of L99P in Racket

--- Languages/Racket/L99Problems/prob017-020.rkt
+++ Languages/Racket/L99Problems/prob017-020.rkt
@@ -1,5 +1,6 @@
 ;;;; Problems from 17 to 20
 
+#lang racket
 
 ;; Problem 017 Split a list in two
 ;; the length of the first one is given as input
@@ -18,3 +19,18 @@
 ;; Prob 018 extract a slice from a list 
 ;; Return slice of list from i to k included
 ;; Count elements from 1 in list
+(define (my-slice lst index1 index2)
+  (define (help-rec l i1 i2 acc)
+    (cond [(and (zero? i1) (zero? i2)) acc]
+	  [(> i1 1) (help-rec (cdr l) (- i1 1) (- i2 1) acc)]
+	  [(zero? i1) (help-rec (cdr l) i1 (- i2 1) (cons (car l) acc))]
+	  [else (help-rec (cdr l) (- i1 1) (- i2 1) (cons (car l) acc))]))
+  (if (or (<= index1 0) (>= index2 (length lst)))
+      (displayln "Index error")
+      (reverse (help-rec lst index1 index2 '()))))
+
+;; Test -> OK
+;; (my-slice '(a b c d e f g h i k) 3 7)
+
+		     
+		     
