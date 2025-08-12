0e751efa518e5b11242ba22f155393f7af84448e
drasken <contumax90@yahoo.it>
2025-08-12 17:22:15 +0200

    Problem 025 of L99P in Racket

--- Languages/Racket/L99Problems/prob025-028.rkt
+++ Languages/Racket/L99Problems/prob025-028.rkt
@@ -1,13 +1,14 @@
 #lang racket
-(require "./prob021-024-DONE.rkt")
+
 ;;;; Problems frmo 025 to 028 L99P
 
-(define (my-rnd-select1 lst num) ; from problem 023
-  (define (helper-re l n acc)
-    (cond [(empty? l) acc]
-	  [(zero? n) acc]
-	  [else (helper-re (cdr l) (- n 1) (cons (car l) acc))]))
-  (helper-re (shuffle lst) num '()))
 
+;; Problem 025 random permutation of list elements
+(define (my-rnd-permutation lst)
+  (for/list ([ind (shuffle (range (length lst)))])
+    (list-ref lst ind)))
+
+;; Test
+;; (my-rnd-permutation '(a b c d e f g h))
 
 
