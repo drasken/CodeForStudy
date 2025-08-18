6c1142ec9a6da6a7d084cbfae3fa4b4cd7ded397
drasken <contumax90@yahoo.it>
2025-08-18 22:23:51 +0200

    Advanced with vector solution

--- Exercises/Euler/euler0019.rkt
+++ Exercises/Euler/euler0019.rkt
@@ -10,10 +10,11 @@
 ;; Using this algorithm https://it.wikipedia.org/wiki/Calendario_perpetuo
 
 ;; Utility function for leap years
-;; (define (is-leap? num)
-;;   (cond [(and (zero? (remainder num 100)) (not (zero? (remainder num 400)))) #f]
-;; 	[(zero? (remainder num 4)) #t]
-;; 	[else #f]))
+(define (is-leap? num)
+  (cond [(and (zero? (remainder num 100)) (not (zero? (remainder num 400)))) #f]
+	[(zero? (remainder num 4)) #t]
+	[else #f]))
+
 
 
 ;; Try 001: using matrix and checking with offset modulo 7
@@ -41,12 +42,30 @@
   (for* ([i (in-range (vector-length calendar))]
 	 [j (in-range 1 13)] ; iterate on all matrix
 	 #:unless (and (zero? i)(zero? j)))
-    (printf  "test ~a ~a \t" i j)))
+    (vector-m
+    ))
+;    (printf  "test ~a ~a \t" i j)))
+    
 ;; TODO finish to implement calculation based on previous index
 ;; add auxiliary function
 
+;; Calc day value starting from Sunday as zero
+(define (calculate-day array-year index-month)
+  (define my-calc (lambda (x) (remainder x 7)))
+  (let ([year (vector-ref array-year 0)]
+	[day (vector-ref array-year index-month)])
+    (cond [(member day '(1 3 5 7 8 10 12)) (my-calc 31)]
+	  [(member day '(4 6 9 11)) (my-calc 30)]
+	  [(is-leap? year) (my-calc 29)]
+	  [else (my-calc 28)])))
 
-;; This will be maybe my main funciton
-;; (define my-calendar (for/vector ([i (in-range 1900 2001)])
-;; 		      (build-vector
 
+
+;; This will be maybe my main funciton to get problem solution
+(define (main year1 year2)
+  (let ([calendar (init-calendar (add1 (- year2 year1)))])
+    (set-calendar-init-state calendar year1)
+    calendar))
+
+
+(main 1900 1902)
