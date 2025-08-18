1343543eff96b633a411bbe770204b135958699a
drasken <contumax90@yahoo.it>
2025-08-18 23:48:06 +0200

    Started second try finding solution with list

--- Exercises/Euler/euler0019.rkt
+++ Exercises/Euler/euler0019.rkt
@@ -17,55 +17,68 @@
 
 
 
-;; Try 001: using matrix and checking with offset modulo 7
+;; Try 02 with list
 
-(define (init-matrix years elements)
-  (lambda (x)
-    (for/vector ([i (in-range x)])
-      (for/vector ([j (in-range elements)])
-	-1))))
+;; Algo: add to alist of number modulo 7 depending on
+;; the previous added value and the step counter for moths
+;; using a second counter for year and lep year count
 
-(define (init-calendar years)
-  (let ([my-calendar (init-matrix years 13)])
-    (my-calendar years)))
+;; create a list with index and populate it until year is surpassed
+;; (define (my-calendar start-year end-year)
+;;   (define (helper-rec year1 year2 month acc)
+;;     (cond [
 
 
-(define (set-calendar-init-state calendar year)
-  (let ([val year]
-	[first #t])
-    (for ([i calendar])
-      (vector-set! i 0 val)
-      (set! val (add1 val)))
-    (vector-set! (vector-ref calendar 0) 1 0))) ; hardcoded firt day is a sunday
 
-(define (set-calendar-first-days calendar)
-  (for* ([i (in-range (vector-length calendar))]
-	 [j (in-range 1 13)] ; iterate on all matrix
-	 #:unless (and (zero? i)(zero? j)))
-    (vector-m
-    ))
-;    (printf  "test ~a ~a \t" i j)))
+;; ;; Try 001: using matrix and checking with offset modulo 7
+
+;; (define (init-matrix years elements)
+;;   (lambda (x)
+;;     (for/vector ([i (in-range x)])
+;;       (for/vector ([j (in-range elements)])
+;; 	-1))))
+
+;; (define (init-calendar years)
+;;   (let ([my-calendar (init-matrix years 13)])
+;;     (my-calendar years)))
+
+
+;; (define (set-calendar-init-state calendar year)
+;;   (let ([val year]
+;; 	[first #t])
+;;     (for ([i calendar])
+;;       (vector-set! i 0 val)
+;;       (set! val (add1 val)))
+;;     (vector-set! (vector-ref calendar 0) 1 0))) ; hardcoded firt day is a sunday
+
+;; (define (set-calendar-first-days calendar)
+;;   (for* ([i (in-range (vector-length calendar))]
+;; 	 [j (in-range 1 13)] ; iterate on all matrix
+;; 	 #:unless (and (zero? i)(zero? j)))
+;;     (vector-m
+;;     ))
+;; ;    (printf  "test ~a ~a \t" i j)))
     
-;; TODO finish to implement calculation based on previous index
-;; add auxiliary function
+;; ;; TODO finish to implement calculation based on previous index
+;; ;; add auxiliary function
 
-;; Calc day value starting from Sunday as zero
-(define (calculate-day array-year index-month)
-  (define my-calc (lambda (x) (remainder x 7)))
-  (let ([year (vector-ref array-year 0)]
-	[day (vector-ref array-year index-month)])
-    (cond [(member day '(1 3 5 7 8 10 12)) (my-calc 31)]
-	  [(member day '(4 6 9 11)) (my-calc 30)]
-	  [(is-leap? year) (my-calc 29)]
-	  [else (my-calc 28)])))
-
-
-
-;; This will be maybe my main funciton to get problem solution
-(define (main year1 year2)
-  (let ([calendar (init-calendar (add1 (- year2 year1)))])
-    (set-calendar-init-state calendar year1)
-    calendar))
+;; ;; Calc day value starting from Sunday as zero
+;; (define (calculate-day array-year index-month)
+;;   (define my-calc (lambda (x) (remainder x 7)))
+;;   (let ([year (vector-ref array-year 0)]
+;; 	[day (vector-ref array-year index-month)])
+;;     (cond [(member day '(1 3 5 7 8 10 12)) (my-calc 31)]
+;; 	  [(member day '(4 6 9 11)) (my-calc 30)]
+;; 	  [(is-leap? year) (my-calc 29)]
+;; 	  [else (my-calc 28)])))
 
 
-(main 1900 1902)
+
+;; ;; This will be maybe my main funciton to get problem solution
+;; (define (main year1 year2)
+;;   (let ([calendar (init-calendar (add1 (- year2 year1)))])
+;;     (set-calendar-init-state calendar year1)
+;;     calendar))
+
+
+;; (main 1900 1902)
