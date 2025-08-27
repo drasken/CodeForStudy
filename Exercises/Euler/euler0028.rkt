28d07a48f0709c0f21473afc5776df7b8a682e70
drasken <contumax90@yahoo.it>
2025-08-27 23:45:04 +0200

    Started ProjectEuler problem 0028

---
+++ Exercises/Euler/euler0028.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+;; Find sum of number on matrix diagonals
+;; the matrix is created from center and spiraling counter clockwise
+;; Ex given matrix 5 * 5 -> 101
+;; Find result for a matrix 1001 * 1001
+
+
+;; Idea: create first upper and lower rows and get sum
+
+(define (aux-list side-len el res-list)
+  (cons (list (+ el side-len 1)
+	      (+ el 2 (* 2 side-len))
+	      (+ el 3 (* 3 side-len))
+	      (+ el 4 (* 4 side-len)))
+	res-list))
+
+(define test-acc (list '(3 5 7 9) '(1)))
+;; Test
+;; (aux-list 3 9 test-acc)
+
+;; Function to create list of list of desired number to sum
+(define (create-matrix limit) ; limit is the matrix side dimension
+  (define (helper-rec lim step last-el acc) ; step is starting matrix side dimension
+    (cond [(> step limit) acc]
+	  [else (helper-rec lim (+ 2 step) (+ (* 4 step) 4 last-el) (; TODO
+
