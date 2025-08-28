0ffaeac1b3a892da0f2aa5d78905cfc56a42d396
drasken <contumax90@yahoo.it>
2025-08-28 13:02:17 +0200

    ProjectEuler problem 0028 solved

--- Exercises/Euler/euler0028.rkt
+++ Exercises/Euler/euler0028.rkt
@@ -9,19 +9,24 @@
 ;; Idea: create first upper and lower rows and get sum
 
 (define (aux-list side-len el res-list)
-  (cons (list (+ el side-len 1)
+  (append (list (+ el side-len 1)
 	      (+ el 2 (* 2 side-len))
 	      (+ el 3 (* 3 side-len))
 	      (+ el 4 (* 4 side-len)))
 	res-list))
 
-(define test-acc (list '(3 5 7 9) '(1)))
+(define test-acc (list 3 5 7 9 1))
 ;; Test
 ;; (aux-list 3 9 test-acc)
 
 ;; Function to create list of list of desired number to sum
 (define (create-matrix limit) ; limit is the matrix side dimension
   (define (helper-rec lim step last-el acc) ; step is starting matrix side dimension
-    (cond [(> step limit) acc]
-	  [else (helper-rec lim (+ 2 step) (+ (* 4 step) 4 last-el) (; TODO
+    (cond [(= step limit) acc]
+	  [else (helper-rec lim (+ 2 step) (+ (* 4 step) 4 last-el)
+			    (aux-list step last-el acc))]))
+  (helper-rec limit 1 1 '(1)))
+
+;; Solution -> OK it works!
+(define my-res (apply + (create-matrix 1001)))
 
