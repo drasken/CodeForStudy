4eff3fb533ddbf662e5c682de5040918eb1d08a0
drasken <contumax90@yahoo.it>
2025-09-22 15:54:38 +0200

    Done some work on sudoku solver

--- Languages/Python/SimpleSudokuSolver/main.py
+++ Languages/Python/SimpleSudokuSolver/main.py
@@ -1,3 +1,50 @@
 """
 This will be my main funciton to sudoku solver
+To see further documentation, see notes.org
 """
+
+
+import random
+import pprint  # Useful for debugging
+
+
+# GLOBAL VARIABLES HERE ---------------
+POPULATE_PARAM_EASY: float = 0.80
+POPULATE_PARAM_MEDIUM: float = 0.65
+POPULATE_PARAM_HARD: float = 0.50
+POPULATE_PARAM_VERY_HARD: float = 0.3
+
+
+def init_empty_table() -> list[list]:
+    """
+    Make an initial empty sudoku table.
+    The dimension es the classic 9x9 sudoku
+    """
+    my_table: list[list] = [[0 for x in range(9)] for y in range(9)]
+
+    return my_table
+
+
+def randomize_table(table: list[list]) -> None:
+    """
+    Modify a given table in place
+    """
+    for row_index, row in enumerate(table):
+        for index, elemenet in enumerate(row):
+            if random.random() < POPULATE_PARAM_EASY:  # CHANGE HARD CODED PARAM
+                table[row_index][index] = random.randint(1,9)
+            else:
+                table[row_index][index] = 0  # Be sure to set as empty cell
+
+
+def main():
+    """
+    Main function, my simple app entry point
+    """
+    my_table: list[list] = init_empty_table()
+    randomize_table(my_table)
+
+    return my_table
+
+
+test = main()
